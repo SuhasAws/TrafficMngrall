019f5cce22c81d1ad6c25939243ee17c0fb59eef
SuhasAws <125847984+SuhasAws@users.noreply.github.com>
2023-08-03 23:54:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.67.0"
+    }
+  }
+}
 provider "azurerm" {
   features{}
 }
@@ -5,16 +13,16 @@ provider "azurerm" {
 module "resource-group" {
     source = "./modules/ResourceGroup"
     resource-group-name = var.resource-group-name
-    location = var.location 
+    location = var.location
 }
 
 module "vnet" {
     source = "./modules/VirtualNetwork"
     vnet-name = var.vnet-name
     vnet-address-space = var.vnet-address-space
-    resouce-group-name = modules.ResourceGroup.resource-group-name
-    location = modules.ResourceGroup.location
+    resource-group-name = module.resource-group.resource-group-name
+    location = module.resource-group.location
     subnet-name = var.subnet-name
     subnet-address-prefix = var.subnet-address-prefix
-    
+
 }
