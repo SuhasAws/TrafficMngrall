6ba83bad959e7857f6b76fdf589ce42180ecb285
SuhasAws <125847984+SuhasAws@users.noreply.github.com>
2023-08-05 19:24:03 +0530

    Update variables.tf

--- modules/LoadBalancer/variables.tf
+++ modules/LoadBalancer/variables.tf
@@ -1,7 +1,7 @@
 variable "pip-name" {}
 variable "pip-allocation_method" {}
 variable "LB-name" {}
-variable "LB-FEIP-name" {}
+variable "FEIP-name" {}
 variable "BEPool-name" {}
 variable "LBProbe-name" {}
 variable "LBprobe-port" {}
@@ -11,15 +11,5 @@ variable "LBRule-frontend-port" {}
 variable "LBRule-backend-port" {}
 variable "frontend_ip_configuration_name" {}
 variable "NSG-name" {}
-variable "NSRule-name" {}
-variable "NSRule-priority" {}
-variable "NSRule-direction" {}
-variable "NSRule-access" {}
-variable "NSRule-protocol" {}
-variable "NSRule-source_port_range" {}
-variable "NSRule-destination_port_rangee" {}
-variable "NSRule-source_address_prefix" {}
-variable "NSRule-destination_address_prefix" {}
-variable "resouce-group-name" {}
+variable "resource-group-name" {}
 variable "location" {}
-
