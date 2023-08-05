c70b903d1898f67bbe162c31d5ebedd26942289e
SuhasAws <125847984+SuhasAws@users.noreply.github.com>
2023-08-05 19:23:37 +0530

    Update main.tf

--- modules/LoadBalancer/main.tf
+++ modules/LoadBalancer/main.tf
@@ -1,19 +1,19 @@
 resource "azurerm_public_ip" "pip" {
   name                = var.pip-name
   location            = var.location
-  resource_group_name = var.resouce-group-name   
+  resource_group_name = var.resource-group-name   
   allocation_method   = var.pip-allocation_method
 }
 
 resource "azurerm_lb" "LB" {
   name                = var.LB-name
   location            = var.location
-  resource_group_name = var.resouce-group-name
+  resource_group_name = var.resource-group-name
+}
 
-  frontend_ip_configuration {
-    name                 = var.LB-FEIP-name
+ resource "frontend_ip_configuration" "FEIP" {
+    name                 = var.FEIP-name
     public_ip_address_id = azurerm_public_ip.pip.id
-  }
 }
 
 resource "azurerm_lb_backend_address_pool" "BEPool" {
@@ -39,19 +39,5 @@ resource "azurerm_lb_rule" "LBRule" {
 resource "azurerm_network_security_group" "NSG" {
   name                = var.NSG-name
   location            = var.location
-  resource_group_name = var.resouce-group-name
-}
-
-resource "azurerm_network_security_rule" "NSRule" {
-  name                        = var.NSRule-name
-  priority                    = var.NSRule-priority
-  direction                   = var.NSRule-direction
-  access                      = var.NSRule-access
-  protocol                    = var.NSRule-protocol
-  source_port_range           = var.NSRule-source_port_range
-  destination_port_range      = var.NSRule-destination_port_range
-  source_address_prefix       = var.NSRule-source_address_prefix
-  destination_address_prefix  = var.NSRule-destination_address_prefix
-  resource_group_name         = var.resouce-group-name
-  network_security_group_name = azurerm_network_security_group.NSG.name
+  resource_group_name = var.resource-group-name
 }
