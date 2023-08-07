ac1db331d243eff5f3b8665762e68d23b0483471
SuhasAws <125847984+SuhasAws@users.noreply.github.com>
2023-08-07 16:08:34 +0530

    Update variables.tf

--- modules/VirtualNetwork/variables.tf
+++ modules/VirtualNetwork/variables.tf
@@ -1,6 +1,6 @@
 variable "vnet-name" {}
 variable "vnet-address-space" {}
-variable "subnet-name" {}
-variable "subnet-address-prefix" {}
+variable "vnet-subnet-name" {}
+variable "vnet-subnet-address_prefixes" {}
 variable "resource-group-name" {}
 variable "location" {}
